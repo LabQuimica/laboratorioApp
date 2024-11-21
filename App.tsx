c994639c4ab24222236aa1b7ffcf7df8a28cfb17
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-11-21 01:19:18 -0600

    test solicitud a servidor

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
-console.log("App.tsx");
 export default function App() {
+  const [data, setData] = useState();
+
+  async function fetchData() {
+    try {
+      const response = await fetch("http://192.XXXX:3000/user/2"); //colocar ip de la maquina donde esta corriendo el servidor
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{JSON.stringify(data)}</Text>
     </View>
   );
 }
