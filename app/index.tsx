d7ecd8dbf4b94ba6e70ce6cf701635fb672f274a
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-11-22 10:18:02 -0600

    Adding expo Routes and nativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -4,10 +4,9 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const [data, setData] = useState();
-
   async function fetchData() {
     try {
-      const response = await fetch("http://192.XXXX:3000/user/2"); //colocar ip de la maquina donde esta corriendo el servidor
+      const response = await fetch(`http://192.XXX:3000/user/2`); //colocar ip de la maquina donde esta corriendo el servidor
       const data = await response.json();
       setData(data);
       console.log(data);
@@ -22,18 +21,9 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View className="bg-black flex-1 justify-center ">
       <StatusBar style="auto" />
-      <Text>{JSON.stringify(data)}</Text>
+      <Text className="text-white">{JSON.stringify(data)}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
